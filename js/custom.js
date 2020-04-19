b8b35b4862aeb1df8889add587551989d804b105
utshaweu <mahmudsajib95@gmail.com>
2020-04-19 22:17:23 +0600

    corona html web template

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(function(){
+  'use strict'
+
+  //Sticky Menu js
+  $(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+     
+     if(scrolling > 50){
+         $(".navbar").addClass("menu-bg");
+     }
+     else{
+         $(".navbar").removeClass("menu-bg");
+     }
+     if(scrolling > 100){
+         back2top.fadeIn(500);
+     }
+     else{
+         back2top.fadeOut(500);
+     }
+ });
+
+
+  //Back to top js
+  var back2top = $(".back-to-top");
+  var html_body = $('html,body');
+  back2top.click(function(){
+    html_body.animate({scrollTop:0},1000);
+  });
+
+  //Smooth Scroll js
+  var html_body = $('html, body');
+  $('nav a').on('click', function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+              html_body.animate({
+                  scrollTop: target.offset().top - 0
+              }, 1500);
+              return false;
+          }
+      }
+  });	
+
+});
